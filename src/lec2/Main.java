f767e0c2340ec545908d1f668d4822af2edd34ef
Richard Lu <lu16j@mit.edu>
2018-01-09 03:08:08 -0500

    exercise code

--- src/lec2/Main.java
+++ src/lec2/Main.java
@@ -1,34 +1,35 @@
 package lec2;
 
 public class Main {
-	
+
 	public static void main(String[] args) {
-		
+
 		double milesToTravel = 2.0;
-		
+
 		double uberBasePrice = 2.00;
 		double lyftBasePrice = 2.00;
 		double uberRate = 1.24;
 		double lyftRate = 1.22;
 		double uberServiceFee = 1.15;
 		double lyftServiceFee = 1.70;
-		
-		double uberPrice;
-		double lyftPrice;
+
+		// TODO #1: move this calculation to an `estimateRidePrice` method
+		double uberPrice = uberBasePrice + uberServiceFee + uberRate * milesToTravel;
+		double lyftPrice = lyftBasePrice + lyftServiceFee + lyftRate * milesToTravel;
+
 		double difference;
-		
-		uberPrice = uberBasePrice + uberServiceFee + uberRate * milesToTravel;
-		lyftPrice = lyftBasePrice + lyftServiceFee + lyftRate * milesToTravel;
-		
+
 		if (uberPrice > lyftPrice) {
 			difference = uberPrice - lyftPrice;
-			System.out.printf("You should take a Lyft. You saved $%.2f! \n", difference);
+			System.out.printf("You should take a Lyft. You saved $%.2f!\n", difference);
 		} else if (lyftPrice > uberPrice) {
 			difference = lyftPrice - uberPrice;
-			System.out.printf("You should take a Uber. You saved $%.2f! \n", difference);
+			System.out.printf("You should take a Uber. You saved $%.2f!\n", difference);
 		} else {
 			System.out.println("You can take either an Uber or a Lyft.");
 		}
-		
+
+		// TODO #2: create and populate an array
+
 	}
 }
